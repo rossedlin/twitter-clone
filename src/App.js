0bf52b0b3b0db67226ea43388b2dbf440ac245ce
Ross Edlin <rossedlin@gmail.com>
2021-02-08 17:46:04 +0700

    Add - Main App

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCq1MQ0DTLOK6heIgzLrJ5FnVSKBUCyUc4",
+  authDomain: "twitter-clone-c935e.firebaseapp.com",
+  databaseURL: "https://twitter-clone-c935e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "twitter-clone-c935e",
+  storageBucket: "twitter-clone-c935e.appspot.com",
+  messagingSenderId: "254207818709",
+  appId: "1:254207818709:web:0e5c3aae7d6b5aa3915b5c",
+  measurementId: "G-MV86CVLGXG",
+}***REMOVED***
+
+function App() {
+  return (
+      <div className="App">
+        <header>
+          <h1>Twitter Clone</h1>
+        </header>
+
+        <section>
+        </section>
+  ***REMOVED***
+  ***REMOVED***
+}
+
+export default App;
