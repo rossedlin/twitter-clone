9e60c4e370ab8c15fd09a6fc53c683e87d6d3c54
Ross Edlin <rossedlin@gmail.com>
2021-03-02 14:58:16 +0700

    Add - Hello World

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,20 @@
+***REMOVED***
+
+***REMOVED***
+ *
+ * @return {JSX.Element}
+ * @constructor
+***REMOVED***
+function HelloWorld() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 text-white font-weight-bold text-center" style={{fontSize: 36, marginTop: "50%"}}>
+          Hello World
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+  ***REMOVED***
+}
+
+export default HelloWorld;
