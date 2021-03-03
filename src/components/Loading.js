2ef3aeb2aba794273b64fd1e59122a82b0560da0
Ross Edlin <rossedlin@gmail.com>
2021-03-03 12:35:16 +0700

    Add - Loading.js

---
+++ src/components/Loading.js
@@ -0,0 +1,17 @@
+***REMOVED***
+***REMOVED*** FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+***REMOVED*** faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+function Loading() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 text-white font-weight-bold text-center" style={{fontSize: 36, marginTop: "30%"}}>
+          <FontAwesomeIcon icon={faSpinner} spin />
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+  ***REMOVED***
+}
+
+export default Loading;
