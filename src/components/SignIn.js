3dbd43a29c2b811f7024d528dc7cfb364ae8c979
Ross Edlin <rossedlin@gmail.com>
2021-03-05 11:02:34 +0700

    Update - login to make it UX friendly

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -33,8 +33,26 @@ function SignIn({firebase}) {
             </h1>
       ***REMOVED***
           <div className="col-12 mt-5">
-            <p className="text-info">This is where the normal username / email and password would go,
-              but we're going to login with your google Google Account.</p>
+            <p className="text-info">
+              Hey there, I'm a Twitter Clone.<br/>
+            </p>
+            <p className="text-info">
+              If you view the <a href="https://twitter.com/" target="_blank">twitter</a> website on your mobile phone,
+              you can see I look very similar.
+            </p>
+            <p className="text-info">
+              My purpose is to demonstrate building a <a href="https://firebase.google.com/"
+                                                        target="_blank">firebase</a> / <a href="https://reactjs.org/"
+                                                                                          target="_blank">react</a> app that emulates twitter.
+            </p>
+            <p className="text-info">
+              Normally on the official twitter login you would have a username / password box here.<br/>
+              But instead you don't because we're going to redirect you so you can use your real account to sign in.<br/>
+            </p>
+            <p className="text-info">
+              Don't worry, I'm not stealing your data,
+              you can check my <a href="https://github.com/rossedlin/twitter-clone" target="_blank">source code</a> here if you don't trust me.<br/>
+            </p>
       ***REMOVED***
           <div className="col-12 mt-5">
             <button className="btn btn-primary p-3" style={{width: '100%'}} onClick={signInWithGoogle}>
