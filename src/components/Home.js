103793acd1da2187fcb8ed720e537e897e8d685f
Ross Edlin <rossedlin@gmail.com>
2021-03-07 16:09:06 +0700

    NOT working

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,44 +4,67 @@ import Header from './Header';
 import Tweet from './Tweet';
 import TweetIcon from '../assets/icon/tweet.svg';
 ***REMOVED***useCollectionData} from 'react-firebase-hooks/firestore';
+import HelloWorld from './HelloWorld';
+import Loading from './Loading';
+import Login from '../pages/Login';
 
-***REMOVED***
- *
- * @return {JSX.Element}
- * @constructor
-***REMOVED***
-function Home({firebase, auth}) {
+class Home extends React.Component {
+  constructor(props) {
+    super(props***REMOVED***
+    this.state = {
+      loaded: false,
+      messages: [],
+      name: '',
+    };
+  }
 
-  const firestore = firebase.firestore(***REMOVED***
-
-  const messageRef = firestore.collection('messages'***REMOVED***
-  const query = messageRef.orderBy('createdAt', 'desc').limit(25***REMOVED***
-  const [messages] = useCollectionData(query, {idField: 'id'}***REMOVED***
-
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 fixed-bottom border-light border-top text-center bg-black">
-          <LeftSidebar auth={auth}/>
-    ***REMOVED***
-        <div className="col-12 my-5">
-          <div>
-            {messages && messages.map(msg => <Tweet auth={auth} key={msg.id} message={msg}/>)}
-      ***REMOVED***
-    ***REMOVED***
-        <div className="col-2">
-          <div className="fixed-bottom text-right" style={{marginBottom: '5.5rem', marginRight: '1.5rem'}}>
-            <a href="/compose" className="btn btn-primary rounded-circle p-3">
-              <img src={TweetIcon} className="" width="25" alt="Tweet"/>
-            </a>
-      ***REMOVED***
-    ***REMOVED***
-        <div className="col-12 fixed-top text-white border-light border-bottom bg-black">
-          <RightSidebar auth={auth}/>
-    ***REMOVED***
   ***REMOVED***
-***REMOVED***
+   *
   ***REMOVED***
+  componentDidMount() {
+
+    setTimeout(function() {
+      this.setState({
+        loaded: true,
+        name: 'Stackoverflow'
+      }***REMOVED***
+    }.bind(this), 2000***REMOVED***
+
+    // if (this.loadMessages()) {
+    //   this.state.loaded = true;
+    // }
+  }
+
+  ***REMOVED***
+   *
+   * @returns {*}
+  ***REMOVED***
+  render() {
+    let auth = this.props.firebase.auth(***REMOVED***
+
+    if (this.state.loaded) {
+      return <HelloWorld/>;
+    }
+
+    return <Loading/>;
+  }
+
+  ***REMOVED***
+   *
+   * @returns {*}
+  ***REMOVED***
+  loadMessages() {
+    let firebase        = this.props.firebase;
+    // let auth            = firebase.auth(***REMOVED***
+    let firestore       = firebase.firestore(***REMOVED***
+    let messageRef      = firestore.collection('messages'***REMOVED***
+    let query           = messageRef.orderBy('createdAt', 'desc').limit(25***REMOVED***
+    this.state.messages = useCollectionData(query, {idField: 'id'}***REMOVED***
+
+    return true;
+  }
+
+
 }
 
 export default Home;
