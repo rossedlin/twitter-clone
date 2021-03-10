14d377001d97d5d7890db7ac517c3350b307db9a
Ross Edlin <rossedlin@gmail.com>
2021-03-10 18:42:07 +0700

    Simplify HelloWorld.js

--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import {useAuthState} from 'react-firebase-hooks/auth';
-import {Redirect} from 'react-router-dom';
-
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Tweet from '../components/Tweet';
-import TweetIcon from '../assets/icon/tweet.svg';
 
 /**
  *
@@ -13,31 +6,11 @@ import TweetIcon from '../assets/icon/tweet.svg';
  * @constructor
  */
 function HelloWorld({firebase}) {
-
-  let auth   = firebase.auth();
-  let [user] = useAuthState(auth);
-
-  /**
-   * Redirect if no user
-   */
-  if (user == null) {
-    return <Redirect to='/login'/>;
-  }
-
-  let {displayName} = auth.currentUser;
-
   return (
-    <div className="container border">
+    <div className="container">
       <div className="row">
-        <div className="col-12 text-white border-light border-bottom bg-black">
-          <Header auth={auth}/>
-        </div>
         <div className="col-12 my-5 text-white text-center">
           <h1>Hello World</h1>
-          <p>{displayName}</p>
-        </div>
-        <div className="col-12 border-light border-top text-center bg-black">
-          <Footer/>
         </div>
       </div>
     </div>
