5428430265651dcb7acd57d93451d98abd9c4f43
Ross Edlin <rossedlin@gmail.com>
2021-03-22 10:09:11 +0700

    Add deps

---
+++ init.sh
@@ -0,0 +1,9 @@
+
+npx create-react-app twitter-app
+cd twitter-app
+
+yarn add react-router-dom
+yarn add firebase react-firebase-hooks
+yarn add node-sass
+yarn add bootstrap react-bootstrap
+yarn add @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
