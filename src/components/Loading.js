5c5a60a91d718e03433cec2fcccb6be250a1a140
Ross Edlin <rossedlin@gmail.com>
2021-03-23 10:06:43 +0700

    Reformat

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSpinner} from '@fortawesome/free-solid-svg-icons';
 
 function Loading() {
   return (
     <div className="container loading">
       <div className="row">
         <div className="col-12">
-          <FontAwesomeIcon icon={faSpinner} spin />
+          <FontAwesomeIcon icon={faSpinner} spin/>
         </div>
       </div>
     </div>
