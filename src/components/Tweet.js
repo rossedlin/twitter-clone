fb6eb1285fba5a1a8f56f34286adedab4bf894f2
Ross Edlin <rossedlin@gmail.com>
2021-04-05 17:44:55 +0700

    streamline CSS

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -7,19 +7,17 @@ import React from 'react';
  */
 function Tweet({message}) {
 
-  const {username, displayName, text, photoURL, createdAt} = message;
+  const {displayName, text, photoURL, createdAt} = message;
 
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="row border-bottom border-dark">
-          <div className="col-3">
-            <img src={photoURL || 'https://i.stack.imgur.com/wPh0S.jpg'}
-                 className="img img-fluid rounded-circle rounded p-2"
-                 alt="Profile Picture"/>
+    <div className="tweet">
+      <div>
+        <div>
+          <div className="tweet-image">
+            <img src={photoURL || 'https://i.stack.imgur.com/wPh0S.jpg'} alt="Profile Picture"/>
           </div>
-          <div className="col-9 p-2 text-white">
-            <h6>{displayName || 'Unknown'} <span className="text-dark">@{username || 'unknown'} - {createdAt.toDate().toLocaleDateString()}</span></h6>
+          <div className="tweet-text">
+            <h6>{displayName || 'Unknown'} <span> - {createdAt.toDate().toLocaleDateString()}</span></h6>
             <p>{text}</p>
           </div>
         </div>
