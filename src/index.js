1cdca4ffd7abed52ef67918e25dbf237bb149979
Ross Edlin <rossedlin@gmail.com>
2021-12-18 12:20:24 +0700

    Add in proper .env variables

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import reportWebVitals from './reportWebVitals';
  * Firebase
  */
 import firebase from 'firebase/app';
+
 require('firebase/auth');
 require('firebase/firestore');
-
 
 /**
  * Styles
@@ -28,7 +28,16 @@ import Login from './pages/Login';
 import Logout from './pages/Logout';
 import Compose from './pages/Compose';
 
-firebase.initializeApp(require('./.env.json'));
+firebase.initializeApp({
+  'apiKey':            process.env.REACT_APP_API_KEY,
+  'authDomain':        process.env.REACT_APP_AUTH_DOMAIN,
+  'databaseURL':       process.env.REACT_APP_DATABASE_URL,
+  'projectId':         process.env.REACT_APP_PROJECT_ID,
+  'storageBucket':     process.env.REACT_APP_STORAGE_BUCKET,
+  'messagingSenderId': process.env.REACT_APP_MESSAGING_SENDER_ID,
+  'appId':             process.env.REACT_APP_APP_ID,
+  'measurementId':     process.env.REACT_APP_MEASUREMENT_ID,
+});
 
 /**
  * Render
